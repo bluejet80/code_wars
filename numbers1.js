991b1653e8204c691b3558b0d3b407c23f7b739d
bluejet80 <know_jarett@yahoo.com>
2022-02-12 16:35:48 -0600

    leveled up on code wars

--- numbers1.js
+++ numbers1.js
@@ -160,5 +160,78 @@ const isIsogram = str => new Set([...str.toLowerCase()]).size == str.length;
 // end of challenge
 
 //-----------------------------------------------------------
+// Output whether member is Open or Senior , to be senior atleast 55 and handicap greater than 7
+//Example [[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior']
+function openOrSenior(data) {
+  // ...
+  const output = [];
+  for (const [age, hand] of data) {
+    age >= 55 && hand > 7 ? output.push('Senior') : output.push('Open');
+  }
+  return output;
+}
+
+// Another way with an arrow function
+
+function openSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? 'Senior' : 'Open'
+  );
+}
 
 // end of challenge
+
+//-----------------------------------------------------------
+//determine if there are the same number of x's and o's in a string
+
+function countXO(str) {
+  const toLower = str.toLowerCase();
+  let xCount = 0;
+  let yCount = 0;
+  for (const item of toLower) {
+    if (item === 'x') {
+      xCount++;
+    } else if (item === 'o') {
+      yCount++;
+    }
+  }
+  return yCount === xCount;
+}
+
+console.log(countXO('xo'));
+
+// another way using reg exp
+
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
+
+// Another Way
+
+function XO(str) {
+  let [x, o] = [0, 0];
+  for (let i of str.toLowerCase()) {
+    if (i === 'x') x++;
+    if (i === 'o') o++;
+  }
+  return x === o;
+}
+
+// another way with reg exp
+
+function XO(str) {
+  let a = str.replace(/x/gi, ''),
+    b = str.replace(/o/gi, '');
+  return a.length === b.length;
+}
+
+// another way one liner
+function XO(str) {
+  return (
+    str.toLowerCase().split('x').length === str.toLowerCase().split('o').length
+  );
+}
+
+// end of Challenge
