e64dbb8c4ba3ef6785f06ea9f65807f36b2da0af
Jarett Young <know_jarett@yahoo.com>
2022-02-28 18:28:59 -0600

    finished another kata

--- numbers3.js
+++ numbers3.js
@@ -38,6 +38,49 @@ const printStrCase = function (array) {
   for (const item of array) {
     console.log(item.toLowerCase());
     console.log(item.toUpperCase());
-    console.log(item.capitalize());
+    console.log(item.replace(item[0], item[0].toUpperCase()));
   }
 };
+
+// printStrCase(['happy', 'sad', 'funny']);
+
+// end of challenge
+
+//-------------------------
+// provided with a list (array) if integer pairs.
+// The first element of the pair are how many people are getting on the bus
+// The second element is how many people are getting off the bus
+// Your task is to return the number of people on the bus at the end of the last stop
+
+const pear = [
+  [3, 0],
+  [2, 1],
+  [1, 3],
+];
+
+const number = function (busStops) {
+  let people = 0;
+  for (const [on, off] of busStops) {
+    people += on;
+    people -= off;
+  }
+  return people;
+};
+
+console.log(number(pear));
+
+// this passed all the tests
+// here is another way written in one line
+const number1 = busStops => busStops.reduce((p, n) => p + n[0] - n[1], 0);
+// here is another way
+
+const number2 = function (busStops) {
+  let busCounter = 0;
+  for (let busStop of busStops) {
+    busCounter += busStop[0] -= busStop[1];
+  }
+  return busCounter;
+};
+
+// end of challenge
+//------------------------------
